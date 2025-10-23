cb8cfe5597933320642d1903a0b064b1a44370ee
Amalie Habla <amalie.f.kristensen@gmail.com>
2025-10-23 03:01:14 +0000

    calculated daily sales data

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,14 +1,16 @@
-SELECT  
-    A.date_date,
-    COUNT(A.orders_id) AS nb_transactions, 
-    ROUND(SUM(A.revenue), 2) AS daily_revenue, 
-    ROUND(AVG(A.quantity), 2) AS ave_basket, 
-    ROUND(SUM(B.operational_margin), 2) AS operational_margin,
-    SUM((A.purchase_cost), 2) AS total_purchase_cost, 
-    SUM(B.shipping_fee) AS total_shipping_fee    
-FROM {{ref('int_orders_margin')}} AS A
-LEFT JOIN {{ref('int_orders_operational')}} AS B
-ON A.orders_id = B.orders_id
+SELECT
+    date_date AS date_date,
+    COUNT(DISTINCT orders_id) AS nb_transactions,
+    ROUND(SUM(operational_margin), 2) AS operational_margin, 
+    ROUND(SUM(revenue), 2) AS revenue, 
+    ROUND(SUM(revenue)/NULLIF(COUNT(orders_id),2), 2) AS ave_basket, 
+    ROUND(SUM(purchase_cost), 2) AS purchase_cost, 
+    ROUND(SUM(margin), 2) AS margin, 
+    ROUND(SUM(shipping_fee), 2) AS shipping_fee,
+    ROUND(SUM(logcost), 2) AS logcost, 
+    ROUND(SUM(ship_cost), 2) AS ship_cost,
+    ROUND(SUM(quantity), 2) AS total_quantity   
+FROM {{ref('int_orders_operational')}} 
 GROUP BY date_date
 ORDER BY date_date DESC
 
